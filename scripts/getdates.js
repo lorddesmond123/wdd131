d4bf892e886c3bf55a15aa96622a31cabf78cdc3
lorddesmond123 <liambeedesmondhenga@gmail.com>
2026-09-18 17:49:45 +0100

    correction done

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,3 @@
 // Current year
 document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Last modified date
 document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,7 @@
-const mainnav = document.querySelector('.navigation')
+const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
 
 hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle('show');
-	hambutton.classList.toggle('show');
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
 });
